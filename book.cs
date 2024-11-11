c8fce99aaf72c6e34490ffce0a78b78b7d58fc49
AndersRKvamme <rka092024@gmail.com>
2024-11-11 13:27:22 +0100

    Library Test

---
+++ book.cs
@@ -0,0 +1,16 @@
+using System.Net;
+
+class Book {
+    //  Data fields
+        string Title;
+
+        string Author;
+
+        DateTime FirstPublished;
+
+    //  Constructor for the Class, decides how to construct a valid object
+    public Book (string Title, string Author, DateTime FirstPublished)
+    {
+        
+    } 
+}
