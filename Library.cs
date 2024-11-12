bab8daeac2b38aa353afca669623d6ac7cedd4b6
AndersRKvamme <rka092024@gmail.com>
2024-11-12 12:46:27 +0100

    vetta faen

--- Library.cs
+++ Library.cs
@@ -4,13 +4,15 @@ class Library{
 
     public Library() {
         books = new List<Book>();
-        Book martian = new Book("Martian", "Jane Doe", new DateTime(2004, 10, 10));
-        Book kodehode = new Book("Kodehode", "Kode Hode", new DateTime(2001, 11, 09));
-        Book martian2 = new Book("Martian 2", "Jane Doe", new DateTime(2014, 10, 10));
+        books martian = new Book("Martian", "Jane Doe", new DateTime(2004, 10, 10));
+        books kodehode = new Book("Kodehode", "Kode Hode", new DateTime(2001, 11, 09));
+        books martian2 = new Book("Martian 2", "Jane Doe", new DateTime(2014, 10, 10));
     }
-    public Book LendBook (string title) {
-        books.Find ((book) => {
-            if (book.Title == title){
+    public Book? LendBook (string title) {
+        Book? book = books.Find((book) => 
+        {
+            if (book.Title == title)
+            {
                 return true;
             }
             else
@@ -19,10 +21,12 @@ class Library{
             }
         });
 
-        if (book == null) {
-            return "Did not find book";
+        if (book == null) 
+        {
+            return null;
         }
         else 
+        
         {
             return book;
         }
